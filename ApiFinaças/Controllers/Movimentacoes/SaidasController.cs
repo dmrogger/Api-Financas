7d4c491f39013e9684677aaa0ebea6ea47793673
roger <roger@DESKTOP-PSIMH45>
2025-01-21 20:02:32 -0300

    Adicionar arquivos de projeto.

---
+++ ApiFinaças/Controllers/Movimentacoes/SaidasController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiFinaças.Controllers.Movimentacoes
+{
+    [ApiController]
+    [Route("/saida")]
+    public class SaidasController : ControllerBase
+    {
+        [HttpGet]
+        public Task<IActionResult> ObterSaidas()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public Task<IActionResult> AdicionarSaidas()
+        {
+            return null;
+        }
+
+        [HttpDelete]
+        public Task<IActionResult> ExcluirSaidas()
+        {
+            return null;
+        }
+    }
+}
