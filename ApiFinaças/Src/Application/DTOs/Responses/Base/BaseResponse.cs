a289c2d02056277271ba0e03d833f57679edbe13
Roger Daniel Maximiliano <rogerdaniel2000@outlook.com>
2026-01-12 21:14:26 -0300

    Adequação das responses e criação do repository

---
+++ ApiFinaças/Src/Application/DTOs/Responses/Base/BaseResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace ApiFinancas.Src.Application.DTOs.Responses.Base
+{
+    public class BaseResponse<T>
+    {
+        public bool Sucess {  get; set; }
+        public string? Message { get; set; }
+        public T? Data { get; set; }
+        public List<string>? Errors { get; set; }
+        public string TraceId { get; set; } = Guid.NewGuid().ToString();
+        public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
+
+
+        public static BaseResponse<T> Ok(T data, string? message = null) 
+            => new() { Sucess = true, Message = message };
+
+        public static BaseResponse<T> Fail(List<string> errors, string? message = null)
+            => new() { Sucess =  false, Errors = errors, Message = message };
+
+        public static BaseResponse<T> Fail(string error, string? message = null)
+            => new() { Sucess = false, Errors = new List<string> { error }, Message = message };
+
+    }
+}
--- ApiFinaças/Src/Presentation/Controllers/Usuario/UsuarioController.cs
+++ ApiFinaças/Src/Presentation/Controllers/Usuario/UsuarioController.cs
@@ -1,14 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiFinaças.Src.Application.DTOs.Requests.Usuario;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace ApiFinaças.Src.Presentation.Controllers.Usuario
 {
-    public class UsuarioController : ControllerBase
+    [ApiController]
+    [Route("api/v1/usuarios")]
+    [Produces("application/json")]
+    public class UsuariosController : ControllerBase
     {
-        [HttpPost("Usuario")]
-        public async Task<IActionResult> CriarUsuario()
+        /// <summary>
+        /// Cria um novo usuário
+        /// </summary>
+        [HttpPost("usuario")]
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        public async Task<IActionResult> CriarUsuario([FromBody]CriaUsuarioRequest request, 
+                                                      CancellationToken cancellationToken)
         {
-            return null;
+            return Created(string.Empty, null);
         }
-      
+
+        /// <summary>
+        /// Consulta um usuário por id
+        /// </summary>
+        [HttpGet("usuario{id:guid}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> ConsultarUsuario(Guid id)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Remove um usuário
+        /// </summary>
+        [HttpDelete("usuario{id:guid}")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        public async Task<IActionResult> DeletarUsuario(Guid id)
+        {
+            return NoContent();
+        }
     }
 }
