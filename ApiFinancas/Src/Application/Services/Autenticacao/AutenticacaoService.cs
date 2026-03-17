87ca3bc36aecf936dbd79c51e19cd2d3ac030ddc
Roger Daniel <rogerdaniel2000@hotmail.com>
2026-03-16 23:55:09 -0300

    Feat: Implementação dos testes unitários

--- ApiFinancas/Src/Application/Services/Autenticacao/AutenticacaoService.cs
+++ ApiFinancas/Src/Application/Services/Autenticacao/AutenticacaoService.cs
@@ -1,4 +1,5 @@
 ﻿using ApiFinancas.Src.Application.DTOs.Autenticacao;
+using ApiFinancas.Src.Application.DTOs.Common;
 using ApiFinancas.Src.Application.DTOs.Responses.Usuario;
 using ApiFinancas.Src.Application.Interfaces.Autenticacao;
 using ApiFinancas.Src.Domain.Entities;
@@ -24,24 +25,26 @@ namespace ApiFinancas.Src.Application.Services.Autenticacao
             _configuration = configuration;
         }
 
-        public async Task<LoginResponse> LoginAsync(LoginRequest request)
+        public async Task<Result<LoginResponse>> LoginAsync(LoginRequest request)
         {
             var usuario = await _usuarioRepository.ObterPorEmailAsync(request.Email);
-            
+
             if (usuario == null)
-                throw new Exception("Usuário ou senha inválidos");
+                return Result<LoginResponse>.Fail("Usuário ou senha inválidos");
 
             if (!BCrypt.Net.BCrypt.Verify(request.Senha, usuario.Senha))
-                throw new Exception("Usuário ou senha inválidos");
+                 return Result<LoginResponse>.Fail("Usuário ou senha inválidos");
 
             var token = GerarToken(usuario);
 
-            return new LoginResponse(
+            var response = new LoginResponse(
                 usuario.Id,
                 usuario.Nome,
                 usuario.Email,
                 token
             );
+
+            return Result<LoginResponse>.Ok(response);
         }
 
         private string GerarToken(Usuario usuario)
---
+++ Tests/Application/Services/Autenticacao/AutenticacaoServiceTests.cs
@@ -0,0 +1,88 @@
+﻿using ApiFinancas.Src.Application.DTOs.Autenticacao;
+using ApiFinancas.Src.Application.Services.Autenticacao;
+using ApiFinancas.Src.Domain.Entities;
+using ApiFinancas.Src.Domain.Interfaces;
+using BCrypt.Net;
+using Castle.Core.Configuration;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiFinancas.Tests.Application.Services.Autenticacao
+{
+    public class AutenticacaoServiceTests
+    {
+        private readonly Mock<IUsuarioRepository> _repositoryMock;
+        private readonly AutenticacaoService _autenticaoservice;
+        private readonly Microsoft.Extensions.Configuration.IConfiguration _configuration;
+
+        public AutenticacaoServiceTests()
+        {
+            _repositoryMock = new Mock<IUsuarioRepository>();
+            var settings = new Dictionary<string, string>
+            {
+                { "Jwt:Key", "uma-chave-super-secreta-para-testes" },
+                { "Jwt:Issuer", "ApiFinancas" },
+                { "Jwt:Audience", "ApiFinancasUsers" }
+            };
+
+            _configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(settings)
+                .Build();
+
+            _autenticaoservice = new AutenticacaoService(_repositoryMock.Object, _configuration);
+        }
+
+        [Fact(DisplayName = "Deve retornar login com usuário válido")]
+        public async Task LoginDeveRetornarUsuarioValido()
+        {
+            var senhaHash = BCrypt.Net.BCrypt.HashPassword("123456");
+            var usuario = new Usuario("Teste", "teste@gmail.com", senhaHash);
+
+            _repositoryMock.Setup(x => x.ObterPorEmailAsync(It.IsAny<string>()))
+                .ReturnsAsync(usuario);
+
+            var login = new LoginRequest
+            {
+                Email = "teste@gmail.com",
+                Senha = "123456"
+            };
+
+            var result = await _autenticaoservice.LoginAsync(login);
+
+
+            Assert.True(result.Success);
+            Assert.NotNull(result.Data);
+            Assert.Equal(result.Data.Email, login.Email);
+        }
+
+        [Fact(DisplayName = "Deve retornar um login inválido")]
+        public async Task LoginDeveRetornarInvalido()
+        {
+            var senhaHash = BCrypt.Net.BCrypt.HashPassword("654321");
+            var usuario = new Usuario("Teste", "teste@gmail.com", senhaHash);
+
+            _repositoryMock.Setup(x => x.ObterPorEmailAsync(It.IsAny<string>()))
+                .ReturnsAsync(usuario);
+
+            var login = new LoginRequest
+            {
+                Email = "teste@gmail.com",
+                Senha = "123456" //Senha Incorreta para verificação 
+            };
+
+            var result = await _autenticaoservice.LoginAsync(login);
+
+            var esperado = "Usuário ou senha inválidos";
+
+            Assert.False(result.Success);
+            Assert.NotNull(result.Error);
+            Assert.Equal(result.Error, esperado);
+        }
+    }
+}
